b5fc5cb6f4f2c0ddcf88ac778aa190ff02022cfc
ivanlele <ivanlele402@gmail.com>
2025-02-15 15:55:24 +0200

    init

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "TensorFlowLiteSwift",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(
+            name: "TensorFlowLiteSwift",
+            targets: ["TensorFlowLite"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/rarimo/TensorFlowLiteC.git", .branch("main")),
+    ],
+    targets: [
+        .target(
+            name: "TensorFlowLite",
+            dependencies: ["TensorFlowLiteC"]),
+    ]
+)
